13799c87a1e7bf4956c02c6c521725d918675ef9
James Bailey <git@jdb.name>
2022-09-08 23:36:05 +0000

    add error checking

--- src/MyProvider.js
+++ src/MyProvider.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 const MyProvider = (props) => {
 	let myPromiseResolve;
+	let promiseOutcome = true;
 
 	const myCB = () => {
 		console.log("myCB called");
-		myPromiseResolve();
+		myPromiseResolve(promiseOutcome);
 	}
 
 	const resultPromise = () => {
-		 return new Promise((resolve) => {
-			myPromiseResolve = () => {
-				resolve("resolve");
+		 return new Promise((resolve, error) => {
+			myPromiseResolve = (val) => {
+				if (val) {
+					resolve("resolve");
+				} else {
+					error("oops");
+				}
 			}
 
 			console.log("new promise");
@@ -24,18 +29,27 @@ const MyProvider = (props) => {
 		props.myClass.cb = myCB;
 
 		const myPromise = resultPromise();
-		await myPromise;
+		const res = await myPromise.catch((e) => {
+			console.log(e);
+			return e;
+		});
 
-		console.log("returned from await");
+		console.log(`returned from await with ${res}`);
 	}
 
 	React.useEffect(() => {
+		promiseOutcome = false;
 		asyncCall();
 	});
 
+	function click(val) {
+		promiseOutcome = true;
+		asyncCall();
+	}
+
 	return (
 		<div>
-			<button onClick={asyncCall}>Button</button>
+			<button onClick={click}>Button</button>
 		</div>
 	)
 }
